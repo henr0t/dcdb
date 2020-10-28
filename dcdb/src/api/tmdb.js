028cf10c10456a78791eda05c6a57a3ba679b1d9
henr0t <h.trieu@live.nl>
2020-10-28 11:56:20 +0100

    new branch

--- dcdb/src/api/tmdb.js
+++ dcdb/src/api/tmdb.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
-    Authorization: "insert key",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOGY3MjlkMjYxOThkNWYwMWNhNDA0ZTA5NGZlZjY0NSIsInN1YiI6IjVmODQwMDg2NjA4MmViMDAzNmVlYjk0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aCq1tjVknOsHHCFna78rXJbIXIUbPQJcycjj6YaOuB8",
   },
 });
