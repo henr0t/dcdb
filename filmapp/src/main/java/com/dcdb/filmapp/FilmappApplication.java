3c2a558e24afeb44e520de412e92df72b1c36c90
henr0t <h.trieu@live.nl>
2020-10-28 11:50:46 +0100

    first commit

---
+++ filmapp/src/main/java/com/dcdb/filmapp/FilmappApplication.java
@@ -0,0 +1,13 @@
+package com.dcdb.filmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmappApplication.class, args);
+	}
+
+}
