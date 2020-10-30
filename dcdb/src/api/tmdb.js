e5efe4fbf2b5fc831fee429b8c9dba7a46b249a3
henr0t <h.trieu@live.nl>
2020-10-30 16:04:24 +0100

    Router elements

--- dcdb/src/api/tmdb.js
+++ dcdb/src/api/tmdb.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOGY3MjlkMjYxOThkNWYwMWNhNDA0ZTA5NGZlZjY0NSIsInN1YiI6IjVmODQwMDg2NjA4MmViMDAzNmVlYjk0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aCq1tjVknOsHHCFna78rXJbIXIUbPQJcycjj6YaOuB8",
+    Authorization: "insert key",
   },
 });
