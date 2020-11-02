aec72238bf3476a4fc93864f45b6510dd60eced0
henr0t <h.trieu@live.nl>
2020-11-02 17:25:59 +0100

    added IdCheck to prevent other movies

--- dcdb/src/pages/Movie.js
+++ dcdb/src/pages/Movie.js
@@ -1,13 +1,20 @@
 import React from "react";
 import tmdb from "../api/tmdb";
 import MovieDetails from "../components/MovieDetails";
+import data from "../data/movieList";
 
 class Movie extends React.Component {
   state = { movie: [] };
 
   componentDidMount() {
     tmdb.get("/movie/" + this.props.match.params.movieId).then((response) => {
-      this.setState({ movie: response.data });
+      let idCheck = data.map((movie) => {
+        return movie.movieId;
+      });
+
+      if (idCheck.includes(this.props.match.params.movieId)) {
+        this.setState({ movie: response.data });
+      }
     });
   }
 
